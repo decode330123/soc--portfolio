99816417f4c71d1a1a86089eeacaf256e0d850fa
AR <152577655+decode330123@users.noreply.github.com>
2025-07-09 14:39:25 +0530

    tryiing to fix

--- script.js
+++ script.js
@@ -29,14 +29,4 @@ faders.forEach(fader => {
   appearOnScroll.observe(fader);
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburger = document.getElementById('hamburger');
-  const navLinks = document.getElementById('navLinks');
-
-  if (hamburger && navLinks) {
-    hamburger.addEventListener('click', () => {
-      navLinks.classList.toggle('show');
-    });
-  }
-});
 
